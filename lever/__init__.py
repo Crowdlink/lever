cb1f8f04f5900b50b319e384fba110e548238821
Isaac Cook <isaac@simpload.com>
2014-01-14 14:51:06 -0600

    Moved acl parsing utility into Lever, adapted tests to 2.6 and 3.3, added docstring for acl module

--- lever/__init__.py
+++ lever/__init__.py
@@ -3,3 +3,4 @@ __version__ = '0.1'
 # make the following names available as part of the public API
 from .base import API, LeverException, get_joined, LeverSyntaxError, jsonize
 from .mapper import BaseMapper
+from .acl import build_acl
